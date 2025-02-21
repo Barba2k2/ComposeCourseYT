a674d9048aa69bf653d84386ebbee574d08144c2
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-02-21 13:03:33 -0300

    Textfields, Buttons & Showing Snackbars - Class 7

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,65 +1,66 @@
 package com.example.composecourseyt
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+  @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column(Modifier.fillMaxSize()) {
-        val color = remember {
-          mutableStateOf(Color.Yellow)
-        }
-        ColorBox(
-          Modifier
-            .fillMaxSize()
-            .weight(1f)
-        ) {
-          color.value = it
-        }
-        Box(
-          modifier = Modifier
-            .background(color.value)
-            .weight(1f)
-            .fillMaxSize()
-        ) { }
-      }
+      val scaffoldState = rememberScaffoldState()
+      var textFieldState by remember { mutableStateOf("") }
+      val scope = rememberCoroutineScope()
 
+      Scaffold(
+              modifier = Modifier.fillMaxSize(),
+              scaffoldState = scaffoldState,
+      ) {
+        Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize().padding(horizontal = 30.dp)
+        ) {
+          TextField(
+                  value = textFieldState,
+                  label = { Text("Enter your name") },
+                  onValueChange = { textFieldState = it },
+                  singleLine = true,
+                  modifier = Modifier.fillMaxWidth()
+          )
+          Spacer(modifier = Modifier.height(16.dp))
+          Button(
+                  onClick = {
+                    scope.launch { scaffoldState.showSnackbar("Hello $textFieldState") }
+                  }
+          ) { Text("Please greet me") }
+        }
+      }
     }
   }
-}
-
-@Composable
-fun ColorBox(
-  modifier: Modifier = Modifier,
-  updateColor: (Color) -> Unit
-) {
-
-  Box(
-    modifier = modifier
-      .background(Color.Red)
-      .clickable {
-        updateColor(
-          Color(
-            Random.nextFloat(),
-            Random.nextFloat(),
-            Random.nextFloat(),
-            1f
-          )
-        )
-      },
-  )
 }
