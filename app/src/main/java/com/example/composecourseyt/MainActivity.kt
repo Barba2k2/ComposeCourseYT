b2cb108c96a91bcb93a2ce7ce5e4c57cce68bf96
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-02-20 23:10:45 -0300

    Modifiers

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,17 +20,22 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Row(
+      Column(
         modifier = Modifier
-          .width(300.dp)
-          .fillMaxHeight(0.7f)
-          .background(Color.Green),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically,
+          .background(Color.Green)
+          .fillMaxHeight(0.5f)
+          .fillMaxWidth()
+          .border(5.dp, Color.Magenta)
+          .padding(5.dp)
+          .border(5.dp, Color.Blue)
+          .padding(5.dp)
+          .border(10.dp, Color.Red)
+          .padding(10.dp)
+//          .requiredWidth(300.dp)
       ) {
         Text("Hello")
+        Spacer(modifier = Modifier.height(50.dp))
         Text("World")
-        Text("Hello")
       }
     }
   }
