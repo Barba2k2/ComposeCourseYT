2c63bd5d79df4986d1f8f4a6d28538667e1bbb2d
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-02-21 11:10:12 -0300

    Card with Kermit

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,41 +1,89 @@
 package com.example.composecourseyt
 
+// import android.R
+// import com.example.composecourseyt.R
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column(
-        modifier = Modifier
-          .background(Color.Green)
-          .fillMaxHeight(0.5f)
-          .fillMaxWidth()
-          .border(5.dp, Color.Magenta)
-          .padding(5.dp)
-          .border(5.dp, Color.Blue)
-          .padding(5.dp)
-          .border(10.dp, Color.Red)
-          .padding(10.dp)
-//          .requiredWidth(300.dp)
+      val painter = painterResource(id = R.drawable.img_kermit)
+      val description = "Kermit is playing in the snow"
+      val title = "Kermit is playing in the snow"
+      Box(modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp)) {
+        ImageCard(
+                painter = painter,
+                contentDescription = description,
+                title = title,
+        )
+      }
+    }
+  }
+}
+
+@Composable
+fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
+) {
+  Card(
+          modifier = modifier.fillMaxWidth(),
+          shape = RoundedCornerShape(16.dp),
+          elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+  ) {
+    Box(modifier = Modifier.height(200.dp)) {
+      Image(
+              painter = painter,
+              contentDescription = contentDescription,
+              contentScale = ContentScale.Crop,
+      )
+      Box(
+        modifier =
+          Modifier.fillMaxSize()
+            .background(
+              Brush.verticalGradient(
+                colors = listOf(Color.Transparent, Color.Black),
+                startY = 300f,
+                endY = 0f
+              )
+            )
+      )
+      Box(
+              modifier = Modifier.fillMaxSize().padding(12.dp),
+              contentAlignment = Alignment.BottomStart
       ) {
-        Text("Hello")
-        Spacer(modifier = Modifier.height(50.dp))
-        Text("World")
+        Text(
+                title,
+                style = TextStyle(color = Color.White, fontSize = 16.sp),
+        )
       }
     }
   }
